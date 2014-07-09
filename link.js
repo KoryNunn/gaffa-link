28b587c52576a9119e3625dd5ebf3ec29326e331
KoryNunn <knunn187@gmail.com>
2014-07-09 10:58:21 +1000

    Updated compatibility with gaffa 1.10

--- link.js
+++ link.js
@@ -7,9 +7,6 @@ LinkBehaviour.prototype.target = new Gaffa.Property();
 LinkBehaviour.prototype.source = new Gaffa.Property({
     update: function(behaviour, value){
         behaviour.target.set(value, !behaviour.cleans.value);
-    },
-    sameAsPrevious: function(){
-        return false;
     }
 });
 LinkBehaviour.prototype.cleans = new Gaffa.Property();
